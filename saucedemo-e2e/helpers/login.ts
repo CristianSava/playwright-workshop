3dfdcef0fba9f5f49f30bcb8ff1ea02f63ccdee3
Cristian Sava <cristian.sava@qubiz.com>
2025-09-01 13:07:12 +0300

    update

--- saucedemo-e2e/helpers/login.ts
+++ saucedemo-e2e/helpers/login.ts
@@ -2,7 +2,9 @@ import test, { Page } from '@playwright/test';
 import { testIds } from '../selectors/selectors';
 
 export async function login(page: Page, email: string, password: string) {
-  await page.goto('https://saucedemo.com');
+  await page.goto('https://www.saucedemo.com/', {
+    waitUntil: 'networkidle'
+  });
   await page.locator(testIds.usernameInput).fill(email);
   await page.locator(testIds.passwordInput).fill(password);
   await page.locator(testIds.loginButton).click();
